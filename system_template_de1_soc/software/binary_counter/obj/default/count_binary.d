0eeaf34817642efda75c03ecf1d7a9a98fee3ee2
gv319 <gv319@ic.ac.uk>
2022-01-18 00:42:29 +0000

    Task 1 Done.

---
+++ system_template_de1_soc/software/binary_counter/obj/default/count_binary.d
@@ -0,0 +1,17 @@
+obj/default/count_binary.o: count_binary.c count_binary.h \
+ ../binary_counter_bsp//HAL/inc/alt_types.h \
+ ../binary_counter_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../binary_counter_bsp//HAL/inc/io.h \
+ ../binary_counter_bsp//HAL/inc/alt_types.h \
+ ../binary_counter_bsp//HAL/inc/sys/alt_irq.h \
+ ../binary_counter_bsp//HAL/inc/nios2.h ../binary_counter_bsp/system.h \
+ ../binary_counter_bsp/linker.h
+count_binary.h:
+../binary_counter_bsp//HAL/inc/alt_types.h:
+../binary_counter_bsp//drivers/inc/altera_avalon_pio_regs.h:
+../binary_counter_bsp//HAL/inc/io.h:
+../binary_counter_bsp//HAL/inc/alt_types.h:
+../binary_counter_bsp//HAL/inc/sys/alt_irq.h:
+../binary_counter_bsp//HAL/inc/nios2.h:
+../binary_counter_bsp/system.h:
+../binary_counter_bsp/linker.h:
